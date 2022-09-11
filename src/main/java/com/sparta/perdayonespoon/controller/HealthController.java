fd95cc5e77ab1d728857940b01d3fe08475cee47
alsgur <qkralsgur721@gmail.com>
2022-09-12 00:46:46 +0900

    무중단 배포스크립트 nginx 추가

---
+++ src/main/java/com/sparta/perdayonespoon/controller/HealthController.java
@@ -0,0 +1,14 @@
+package com.sparta.perdayonespoon.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class HealthController {
+    @GetMapping("/health")
+    public String healthcheck(){
+        return "ssss";
+    }
+}
