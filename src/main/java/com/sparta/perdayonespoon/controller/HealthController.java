acd907d38c7d200f5fcea389d6549d20dba139b2
alsgur <qkralsgur721@gmail.com>
2022-09-12 17:35:13 +0900

    무중단 배포스크립트 nginx 수정

--- src/main/java/com/sparta/perdayonespoon/controller/HealthController.java
+++ src/main/java/com/sparta/perdayonespoon/controller/HealthController.java
@@ -1,10 +1,32 @@
 package com.sparta.perdayonespoon.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
+@RequiredArgsConstructor
+
 public class HealthController {
+    private final Environment env;
+
+
+
+    @GetMapping("/profile")
+    public String gyunny() {
+        List<String> profile = Arrays.asList(env.getActiveProfiles());
+        List<String> realProfiles = Arrays.asList("real1", "real2");
+        String defaultProfile = profile.isEmpty() ? "default" : profile.get(0);
+
+        return profile.stream()
+                .filter(realProfiles::contains)
+                .findAny()
+                .orElse(defaultProfile);
+    }
     @GetMapping("/health")
     public String healthcheck(){
         return "Pleas12334";
