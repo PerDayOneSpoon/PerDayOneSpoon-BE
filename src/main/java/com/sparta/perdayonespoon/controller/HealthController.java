5e569c4d8c50a3aef86fde33b826617a97f49b2c
alsgur <qkralsgur721@gmail.com>
2022-09-12 14:34:22 +0900

    무중단 배포스크립트 nginx 수정

--- src/main/java/com/sparta/perdayonespoon/controller/HealthController.java
+++ src/main/java/com/sparta/perdayonespoon/controller/HealthController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthController {
     @GetMapping("/health")
     public String healthcheck(){
-        return "ParkMinHyeok";
+        return "HealthCheck";
     }
 }
