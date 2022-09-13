73b1ca9fb6e233ad4b132555af01c976f8da644a
alsgur <qkralsgur721@gmail.com>
2022-09-13 17:36:19 +0900

    무중단 배포스크립트 nginx 수정

--- src/main/java/com/sparta/perdayonespoon/controller/HealthController.java
+++ src/main/java/com/sparta/perdayonespoon/controller/HealthController.java
@@ -30,6 +30,6 @@ public class HealthController {
     }
     @GetMapping("/health")
     public ResponseEntity<String> healthcheck(){
-        return ResponseEntity.ok("R13133?");
+        return ResponseEntity.ok("R13aaaaaaa133?");
     }
 }
