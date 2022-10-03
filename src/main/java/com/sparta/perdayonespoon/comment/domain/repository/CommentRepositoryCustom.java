de57d808b3124ada8a6c3a7a477128b6a0f792ef
User <alstjq8251@gmail.com>
2022-10-03 19:05:18 +0900

    웹훅 url 수정 , 프로필 수정 시 댓글 수정 기능 추가

--- src/main/java/com/sparta/perdayonespoon/comment/domain/repository/CommentRepositoryCustom.java
+++ src/main/java/com/sparta/perdayonespoon/comment/domain/repository/CommentRepositoryCustom.java
@@ -2,10 +2,13 @@ package com.sparta.perdayonespoon.comment.domain.repository;
 
 import com.sparta.perdayonespoon.comment.domain.entity.Comment;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CommentRepositoryCustom {
 
 
     Optional<Comment> getCommentById(Long commentId);
+
+    List<Comment> getCommentByMemberId(Long id);
 }
