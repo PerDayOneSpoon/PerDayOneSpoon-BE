06ad2659ed961ca13d2090c6ca7d95d7a35aabaf
User <alstjq8251@gmail.com>
2022-10-03 21:29:43 +0900

    인싸 뱃지 로직 수정

--- src/main/java/com/sparta/perdayonespoon/comment/dto/CommentRequestDto.java
+++ src/main/java/com/sparta/perdayonespoon/comment/dto/CommentRequestDto.java
@@ -5,9 +5,14 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class CommentRequestDto {
+
+    @NotBlank(message = "댓글 작성시 내용을 입력해야 합니다.")
     private String content;
+
 }
