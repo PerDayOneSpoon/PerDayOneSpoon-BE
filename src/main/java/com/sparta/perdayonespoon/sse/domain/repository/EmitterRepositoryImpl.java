40750e3efd12f66981c60709c983f865e1bb9735
User <alstjq8251@gmail.com>
2022-10-07 01:14:04 +0900

    바꾸기

--- src/main/java/com/sparta/perdayonespoon/sse/domain/repository/EmitterRepositoryImpl.java
+++ src/main/java/com/sparta/perdayonespoon/sse/domain/repository/EmitterRepositoryImpl.java
@@ -38,16 +38,20 @@ public class EmitterRepositoryImpl implements EmitterRepository {
 
     @Override // 구분자를 회원 ID를 사용하기에 StartWith를 사용 - 회원과 관련된 모든 Emitter를 찾는다.
     public Map<String, SseEmitter> findAllEmitterStartWithByMemberId(String userId) {
+        int size = userId.length();
         return emitters.entrySet().stream() // key / value entry 리턴
                 .filter(entry -> entry.getKey().startsWith(userId))
+                .filter(entry->entry.getKey().substring(0,size).equals(userId))
                 // 해당 userId 로 시작하는 키값을 필터 key, value 리턴
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
 
     @Override // 회원에게 수신된 모든 이벤트를 찾는다.
     public Map<String, Object> findAllEventCacheStartWithByMemberId(String userId) {
+        int size = userId.length();
         return eventCache.entrySet().stream()
-                .filter(entry -> entry.getKey().startsWith(userId)).distinct()
+                .filter(entry -> entry.getKey().startsWith(userId))
+                .filter(entry->entry.getKey().substring(0,size).equals(userId))
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
 
@@ -60,10 +64,12 @@ public class EmitterRepositoryImpl implements EmitterRepository {
     // id와 관련된 Emitter를 모두 제거
     @Override
     public void deleteAllEmitterStartWithId(String userId) {
+        int size = userId.length();
         emitters.forEach(
                 (key,emitter) -> {
-                    if(key.startsWith(userId)){
-                        emitters.remove(key);
+                    if(key.startsWith(userId)) {
+                        if(key.substring(0,size).equals(userId))
+                            emitters.remove(key);
                     }
                 }
         );
@@ -71,10 +77,12 @@ public class EmitterRepositoryImpl implements EmitterRepository {
     // id와 관련된 모든 이벤트 캐시를 삭제
     @Override
     public void deleteAllEventCacheStartWithId(String userId) {
+        int size = userId.length();
         eventCache.forEach(
                 (key,emitter) -> {
                     if(key.startsWith(userId)){
-                        eventCache.remove(key);
+                        if(key.substring(0,size).equals(userId))
+                            eventCache.remove(key);
                     }
                 }
         );
